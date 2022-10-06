15b59d3e74cc105f222dd035447a4ece75b67b38
Elena Vorontsova <lena.voroncova26@yandex.ru>
2022-10-06 16:16:23 +0400

    add: add labels to debugViewController

--- RickAndMortyApp/Modules/DebugMenu/DebugMenuView.swift
+++ RickAndMortyApp/Modules/DebugMenu/DebugMenuView.swift
@@ -8,24 +8,70 @@
 import Foundation
 import UIKit
 
+enum DebugConstants {
+    static let topLabels = 10
+}
+
 final class DebugMenuView: UIViewController {
     private lazy var titleLabel: UILabel = {
         let title = UILabel()
         title.text = "Debug Menu"
-        title.textColor = .green
+        title.textColor = .black
+        title.font = .systemFont(ofSize: 28)
+        title.textAlignment = .center
+        return title
+    }()
+    private lazy var cellsCountLabel: UILabel = {
+        let title = UILabel()
+        title.text = "cellsCountLabel"
+        title.textColor = .black
+        title.font = .systemFont(ofSize: 24)
+        title.textAlignment = .center
+        return title
+    }()
+    private lazy var bitesInCoreDataCountLabel: UILabel = {
+        let title = UILabel()
+        title.text = "bitesInCoreDataCountLabel"
+        title.textColor = .black
+        title.font = .systemFont(ofSize: 24)
+        title.textAlignment = .center
+        return title
+    }()
+    private lazy var memoryCountLabel: UILabel = {
+        let title = UILabel()
+        title.text = "memoryCountLabel"
+        title.textColor = .black
+        title.font = .systemFont(ofSize: 24)
+        title.textAlignment = .center
         return title
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .lightGray
+        view.backgroundColor = .white
         configureConstraints()
     }
     
     private func configureConstraints() {
         view.addSubview(titleLabel)
+        view.addSubview(cellsCountLabel)
+        view.addSubview(bitesInCoreDataCountLabel)
+        view.addSubview(memoryCountLabel)
         titleLabel.snp.makeConstraints {
             $0.trailing.leading.equalToSuperview()
+            $0.top.equalTo(view.safeAreaInsets).offset(DebugConstants.topLabels)
+        }
+        cellsCountLabel.snp.makeConstraints {
+            $0.trailing.leading.equalToSuperview()
+            $0.top.equalTo(titleLabel.snp.bottom).offset(DebugConstants.topLabels)
+        }
+        bitesInCoreDataCountLabel.snp.makeConstraints {
+            $0.trailing.leading.equalToSuperview()
+            $0.top.equalTo(cellsCountLabel.snp.bottom).offset(DebugConstants.topLabels)
+        }
+        memoryCountLabel.snp.makeConstraints {
+            $0.trailing.leading.equalToSuperview()
+            $0.top.equalTo(bitesInCoreDataCountLabel.snp.bottom).offset(DebugConstants.topLabels)
         }
     }
 }
